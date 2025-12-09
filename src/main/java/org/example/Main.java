34067097fb797105eb903ea5f581af495854494b
nargiza <nargizagalieva@gmail.com>
2025-12-09 12:41:17 +0300

    1st task is done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
+        printThreeWords();
     }
+
+    private static void printThreeWords(){
+        String text = "Orange \n" + "Banana \n" + "Apple \n";
+        System.out.println(text);
+    }
+
 }
